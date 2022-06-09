cbc1924a3b30f38faad1234a80039c4c1817d20d
alftunner <alftunner@gmail.com>
2022-06-09 19:01:45 +0300

    Initial commit

---
+++ OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOP;
+using static System.Console;
+
+int[][] array = WorkWithArray.getArrayFromUser();
+int[] resultArray = WorkWithArray.concateAndSortArray(array[0], array[1]);
+foreach (int item in resultArray)
+{
+    Write($"{item} ");
+}
+
+
+
+
+
+
---
+++ OOP/WorkWithArray.cs
@@ -0,0 +1,55 @@
+﻿namespace OOP;
+using static System.Console;
+
+public class WorkWithArray
+{
+    public static int[] initArray()
+    {
+        int a;
+        Write("Введите размерность массива: ");
+        while (!int.TryParse(ReadLine(), out a))
+        {
+            Write("Введите размерность массива: ");
+        }
+    
+        int[] array = new int[a];
+        for (int i = 0; i < a; i++)
+        {
+            int b;
+            Write($"Введите {i+1} - й элемент: ");
+            while (!int.TryParse(ReadLine(), out b))
+            {
+                Write($"Введите {i+1} - й элемент: ");
+            }
+    
+            array[i] = b;
+        }
+    
+        return array;
+    }
+    
+    public static int[][] getArrayFromUser()
+    {
+        int a;
+        Write("Введите количество массивов для создания: ");
+        while (!int.TryParse(ReadLine(), out a))
+        {
+            Write("Введите количество массивов для создания: ");
+        }
+    
+        int[][] array = new int[a][];
+        for (int i = 0; i < a; i++)
+        {
+            array[i] = initArray();
+        }
+    
+        return array;
+    }
+    
+    public static int[] concateAndSortArray(int[] array1, int[] array2)
+    {
+        int[] resultArray = array1.Concat(array2).ToArray();
+        Array.Sort(resultArray);
+        return resultArray;
+    }
+}
